7362ca1e341e64e9d92badb872e1ad62a81ab375
Andrey Nechypurenko <andreynech@gmail.com>
2015-01-10 12:29:20 +0100

    Fix the bug which prevented capture from camera. Add comments.

--- src/testvideo.py
+++ src/testvideo.py
@@ -4,13 +4,23 @@ import sys
 import numpy as np
 import cv2
 
+# cv2.VideoCapture can use file and camera as a video source.
+# If string is passed to constructor, then input is treated as
+# file. If integer is passed, then corresponding connected camera will
+# be used, i.e. 0 is /dev/video0, 1 is /dev/video1, etc.
 
 if len(sys.argv) == 2:
-    video_file = sys.argv[1]
+    if f[:-1] == '/dev/video':
+        # use camera for capture
+        video_file = int(f[10:])
+    else:
+        # using file as an input
+        video_file = f
+    
 else:
-    print('Usage: ./testvideo.py <video_device_file_name>')
+    print('Usage: ./testvideo.py <video_file_name>')
     print('using /dev/video0 as video_device_file_name')
-    video_file = '/dev/video0'
+    video_file = 0
 
 cap = cv2.VideoCapture(video_file)
 
@@ -19,9 +29,14 @@ while(ret and cap.isOpened()):
     ret, frame = cap.read()
 
     if ret and frame is not None:
+        # Make frame grayscale
         #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         #cv2.imshow('frame', gray)
-        frame[10][10] = (0, 0, 255) # BGR - draw red dot in top right corner
+
+        # Draw red dot in top right corner to test how frames are
+        # accessible as numpy arrays
+        frame[10][10] = (0, 0, 255) # OpenCV uses BGR
+        
         cv2.imshow('frame', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -30,6 +45,5 @@ while(ret and cap.isOpened()):
 cv2.waitKey(0)
 
 cap.release()
-
 
 cv2.destroyAllWindows()
