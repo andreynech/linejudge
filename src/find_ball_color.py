789e27eff8a130cae3844fcd2098b8c394e191b2
Andrey Nechypurenko <andreynech@gmail.com>
2015-01-13 10:50:33 -0500

    Increase low pass filter kernel size

--- src/find_ball_color.py
+++ src/find_ball_color.py
@@ -114,7 +114,7 @@ while(ret and cap.isOpened()):
         fgmask = fgbg.apply(frame)
 
         # Remove noise from backgound mask (low pass filter)
-        kernel = np.ones((5,5), np.float32) / 25
+        kernel = np.ones((7,7), np.float32) / 49
         fgmask = cv2.filter2D(fgmask, -1, kernel)
         fgmask = cv2.inRange(fgmask, 200, 255)
         fg_frame = cv2.bitwise_and(frame, frame, mask=fgmask)
